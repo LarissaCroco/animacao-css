fdb4ced90a7976df59dd71c6ce9fedd1c6d3453a
Lucas Akira Uehara <akirauehara@hotmail.com>
2020-01-26 11:08:14 -0300

    feat(Script): Fiz um exemplo para primeiro card

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    var audio = new Audio('./babyshark-acustico.m4a');
-    audio = document.getElementById('audio');
+document.addEventListener('DOMContentLoaded', function () {
 
-    var audioButton = document.getElementsByClassName('audio-button');
+	var audio = new Audio('./babyshark-acustico.m4a');
+	audio = document.getElementById('audio');
 
-    Array.from(audioButton).forEach(btn => {
-        btn.addEventListener('click', playAudio.bind(this, btn));
-    })
- 
-    function playAudio(button){
-        console.log('button => ', button);
+	var audioButton = document.getElementsByClassName('audio-button');
 
-        if(button.src.includes("play-button.svg")){
-            button.src = "./images/pause-button.svg";
-            audio.play();
-        } else{
-            button.src = "./images/play-button.svg";
-            audio.pause();
-        }
-    }
+	Array.from(audioButton).forEach(btn => {
+		btn.addEventListener('click', playAudio.bind(this, btn));
+	})
+
+	function playAudio(button) {
+		if (button.src.includes("play-button.svg")) {
+			button.src = "./images/pause-button.svg";
+			audio.play();
+		} else {
+			button.src = "./images/play-button.svg";
+			audio.pause();
+		}
+	}
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	// Fiz um exemplo para primeiro card
+	(() => {
+		'use strict';
+		const imageBabyShark = document.querySelector('#imgBabyShark');
+		const audioButton = document.querySelector('#audio1');
+
+		imageBabyShark.addEventListener('mouseenter', () => {
+			audioButton.dispatchEvent(new Event('click'));
+		});
+
+		imageBabyShark.addEventListener('mouseleave', () => {
+			audioButton.dispatchEvent(new Event('click'));
+		})
+	})();
 });
